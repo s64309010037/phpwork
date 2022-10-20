61673deb8f2b2906910460706cbd2c2bccbb1ae9
s64309010037 <s64309010037@ptnicec.ac.th>
2022-10-20 16:03:13 +0700

    ไฟล์ variable/test

---
+++ variable.php
@@ -0,0 +1,19 @@
+<?php
+    $fname = 2;
+    $a1111 = 4; #ตัวแปร ถ้าตัวแปรซำ้กันจะออกตัวสุดท้าย
+    $_test = 3.33;
+    $Age = 100;
+    $age = 99;
+    $fullname = "Areenee Waemamu";
+    $prefix 
+    = "Mr.";
+
+
+    echo $fname."<br>"; //ข้อความกับตัวแปรต้องมีจุดคั่นไว้
+    echo $a1111;
+    echo "<br>".$Age."<br>";
+    echo $prefix.$fullname."<br>";
+    echo $Age." + ".$age." = ".($Age + $age);
+    print "<br>".$_test;
+
+?>
