a0c8ddd73ea268f634609aade163c0ca29807d14
s64309010037 <s64309010037@ptnicec.ac.th>
2022-10-20 16:00:58 +0700

    ไฟล์ variable/test

---
+++ test.php
@@ -0,0 +1,30 @@
+<?php
+    echo "Areenee Waemamu <br>";
+    //echo "Thailand"; การคอมเมนต์บรรทัดเดียว
+    #echo "Asia"; การคอมเมนต์บรรทัดเดียว
+    
+    /* การคอมเมนต์หลายบรรทัด
+    echo "world <br>";
+    echo "Universe <br>";
+    */
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Information Technology <br>
+    <?php
+    echo "Pattani";
+
+
+
+?>
+</body>
+</html>
