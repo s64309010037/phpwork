62a1a84731b014f50705c67b5bf9752ae00c0fbc
s64309010037 <s64309010037@ptnicec.ac.th>
2022-10-20 16:05:28 +0700

    ส่ง function string

---
+++ function_string.php
@@ -0,0 +1,29 @@
+<?php
+    $name = "Areenee Waemamu";
+    echo strlen($name);
+    echo "<br>";
+    $word = "I love you";
+    echo str_word_count($word);
+    echo "<br>";
+    echo strrev($name);
+    echo "<br>";
+    echo strpos($word , "love");
+    echo "<br>";
+    echo strpos("love" , "hate",$word);
+    echo "<br>";
+    echo strtoupper($word);
+    echo "<br>";
+    echo strtolower($word);
+    echo "<br>";
+    echo substr($word,-3);
+    echo "<br>";
+    echo substr($word,2,4);
+
+
+
+
+
+
+
+
+?>
