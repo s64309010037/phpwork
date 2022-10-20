2ba12018e911e8c7310321197dd5a6fbd618df22
s64309010037 <s64309010037@ptnicec.ac.th>
2022-10-20 16:04:07 +0700

    ส่ง datatype

---
+++ datatype.php
@@ -0,0 +1,32 @@
+<?php
+    $grade = "A";
+    $subject = "HTML";
+    $name = "Areenee";
+    $age = "22";
+
+    //Integer Number
+    $timeout = 30;
+    $scor = 99;
+    $loan = -19;
+    $money = +88;
+
+    //Float point
+    $gpa = 3.33;
+    $insts = 0.25;
+    $temp = -2.22;
+    $lumen = +7600.99;
+    $maths = 2e10;
+    echo $maths;
+
+    //Boolean
+    $famale = true;
+    $male = false;
+
+
+
+
+
+
+
+
+?>
